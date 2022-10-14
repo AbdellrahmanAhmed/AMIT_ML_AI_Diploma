7bab2d9bf1bc072b5f650020f06ab814fd64edaa
Abdellrahman Ahmed <a.ahmed.abdaim@gmail.com>
2022-10-14 23:10:33 +0200

    initial commit

--- note session 06/vectors.py
+++ note session 06/vectors.py
@@ -1,14 +1,16 @@
-List01 = [1, 2, 3]
+List01 = [1,  3]
 List02 = [4, 5, 6]
 
 
 def multipliesTwoVectors(List01, List02):
     List = []
-    assert len(List01) == len(List02)
-    for i in range(0, len(List01)):
-        v = List01[i] * List02[i]
-        List.append(v)
-    print(sum(List))
+    if len(List01) != len(List02):
+        print("Error Length")
+    else:
+        for i in range(len(List01)):
+            v = List01[i] * List02[i]
+            List.append(v)
+        print(sum(List))
 
 
 multipliesTwoVectors(List01=List01, List02=List02)
