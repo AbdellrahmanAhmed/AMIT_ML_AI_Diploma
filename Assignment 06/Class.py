4fb95a27567dadecc0a807e3c615f4544e3cb015
Abdellrahman Ahmed <a.ahmed.abdaim@gmail.com>
2022-10-14 20:40:27 +0200

    initial commit

--- Assignment 06/Class.py
+++ Assignment 06/Class.py
@@ -1,3 +1,12 @@
+#            ____  _____  ______ _      _____            _    _ __  __          _   _            _    _ __  __ ______ _____             ____  _____  ______ _      _____          _____ __  __
+#      /\   |  _ \|  __ \|  ____| |    |  __ \     /\   | |  | |  \/  |   /\   | \ | |     /\   | |  | |  \/  |  ____|  __ \      /\   |  _ \|  __ \|  ____| |    |  __ \   /\   |_   _|  \/  |
+#     /  \  | |_) | |  | | |__  | |    | |__) |   /  \  | |__| | \  / |  /  \  |  \| |    /  \  | |__| | \  / | |__  | |  | |    /  \  | |_) | |  | | |__  | |    | |  | | /  \    | | | \  / |
+#    / /\ \ |  _ <| |  | |  __| | |    |  _  /   / /\ \ |  __  | |\/| | / /\ \ | . ` |   / /\ \ |  __  | |\/| |  __| | |  | |   / /\ \ |  _ <| |  | |  __| | |    | |  | |/ /\ \   | | | |\/| |
+#   / ____ \| |_) | |__| | |____| |____| | \ \  / ____ \| |  | | |  | |/ ____ \| |\  |  / ____ \| |  | | |  | | |____| |__| |  / ____ \| |_) | |__| | |____| |____| |__| / ____ \ _| |_| |  | |
+#  /_/    \_\____/|_____/|______|______|_|  \_\/_/    \_\_|  |_|_|  |_/_/    \_\_| \_| /_/    \_\_|  |_|_|  |_|______|_____/  /_/    \_\____/|_____/|______|______|_____/_/    \_\_____|_|  |_|
+
+
+
 class Dog:
     num_dogs = 0
 
@@ -6,11 +15,30 @@ class Dog:
         self.breed = dogBreed
         self.eyeColor = dogEyeColor
 
+    # Class Method
+    @classmethod
+    def printNumberOfDog(cls):
+        print(Dog.num_dogs)
+
 
 class Student:
     num_student = 0
+    studentList = []
 
     def __init__(self, name, age, numberOfEnrolled):
-        self.name = name
+        Student.num_student += 1
+        self.name = str(name)
         self.age = age
         self.numberOfEnrolled = numberOfEnrolled
+        # studentList = studentList.append(self.name)
+        Student.studentList.append(self.name)
+
+    # Instance Method
+    def sayMyName(self):
+        print(self.name)
+
+    # Static Method
+    @staticmethod
+    def printStudentName():
+        for i in Student.studentList:
+            print(i)
--- Assignment 06/main.py
+++ Assignment 06/main.py
@@ -1,15 +1,29 @@
+#            ____  _____  ______ _      _____            _    _ __  __          _   _            _    _ __  __ ______ _____             ____  _____  ______ _      _____          _____ __  __
+#      /\   |  _ \|  __ \|  ____| |    |  __ \     /\   | |  | |  \/  |   /\   | \ | |     /\   | |  | |  \/  |  ____|  __ \      /\   |  _ \|  __ \|  ____| |    |  __ \   /\   |_   _|  \/  |
+#     /  \  | |_) | |  | | |__  | |    | |__) |   /  \  | |__| | \  / |  /  \  |  \| |    /  \  | |__| | \  / | |__  | |  | |    /  \  | |_) | |  | | |__  | |    | |  | | /  \    | | | \  / |
+#    / /\ \ |  _ <| |  | |  __| | |    |  _  /   / /\ \ |  __  | |\/| | / /\ \ | . ` |   / /\ \ |  __  | |\/| |  __| | |  | |   / /\ \ |  _ <| |  | |  __| | |    | |  | |/ /\ \   | | | |\/| |
+#   / ____ \| |_) | |__| | |____| |____| | \ \  / ____ \| |  | | |  | |/ ____ \| |\  |  / ____ \| |  | | |  | | |____| |__| |  / ____ \| |_) | |__| | |____| |____| |__| / ____ \ _| |_| |  | |
+#  /_/    \_\____/|_____/|______|______|_|  \_\/_/    \_\_|  |_|_|  |_/_/    \_\_| \_| /_/    \_\_|  |_|_|  |_|______|_____/  /_/    \_\____/|_____/|______|______|_____/_/    \_\_____|_|  |_|
+
+
+
 from Class import *
 
+rox = Dog(dogEyeColor="Blue", dogBreed='Bread')
+simba = Dog(dogEyeColor="Black", dogBreed='No Bread')
 
-rox = Dog(dogEyeColor="Blue",dogBreed='Bread')
-simba = Dog(dogEyeColor="Black",dogBreed='No Bread')
-
-print("This dog is a",rox.breed,"and his eyes are",rox.eyeColor,"number of Dogs in class",Dog.num_dogs)
-print("This dog is a",simba.breed,"and his eyes are",simba.eyeColor,"number of Dogs in class",Dog.num_dogs)
+print("This dog is a", rox.breed, "and his eyes are", rox.eyeColor, "number of Dogs in class", Dog.num_dogs)
+print("This dog is a", simba.breed, "and his eyes are", simba.eyeColor, "number of Dogs in class", Dog.num_dogs)
 
 # names = ['aly','mohammed']
-aly = Student(name='Aly Mohammed',age=20,numberOfEnrolled=6)
-mohammed = Student(name='Mohammed Husam',age=22,numberOfEnrolled=5)
+aly = Student(name='Aly Mohammed', age=20, numberOfEnrolled=6)
+mohammed = Student(name='Mohammed Husam', age=22, numberOfEnrolled=5)
 
-# for i in names:
+aly.sayMyName()
+Dog.printNumberOfDog()
+print(Student.studentList)
+
+# for i in Student.studentList:
 #     print("Student name is",i.name,"age",i.age,"number of Student is")
+
+Student.printStudentName()
